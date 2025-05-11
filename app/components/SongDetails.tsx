15d8eee14dda8e92a73a44757fcf854c0f3d2449
Stijn <risseeuw.stijn@gmail.com>
2025-05-11 10:40:28 +0200

    Fixed bugs

--- app/components/SongDetails.tsx
+++ app/components/SongDetails.tsx
@@ -50,8 +50,8 @@ export default function SongDetails({ song }: { song: Song }) {
                         title="Blue Prince: Can a random puzzle game actually work?"
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin"
-                        allowfullscreen
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
                     ></iframe>
                 </div>
             )}
